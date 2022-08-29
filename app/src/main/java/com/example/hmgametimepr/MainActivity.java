dd4cd9151c51b929d3db02c0af1448a57831a911
David <david.ortiz14@upr.edu>
2022-08-28 23:20:59 -0400

    -Webview corrections

---
+++ app/src/main/java/com/example/hmgametimepr/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hmgametimepr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private WebSettings myWebSettings;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.webview);
+        myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://hmgametimepr.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+    public class mywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()){
+            myWebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
